c458dc60ae47a4d3b95fa2ce1ac88b479ffa4bfb
OBats <orest.bats@gmail.com>
2017-09-05 10:31:33 +0300

    Fixed syncRoomsWithCapacity()

--- js/map.js
+++ js/map.js
@@ -47,7 +47,7 @@
   var timeIn = document.querySelector('#timein');
   var timeOut = document.querySelector('#timeout');
   var housingType = document.querySelector('#type');
-  var roomNumber = document.querySelector('#room_number');
+  var roomsNumber = document.querySelector('#room_number');
   var focusedPinElement;
   var focusedPinContent;
   var selectedPin;
@@ -297,30 +297,33 @@
 
   function syncRoomsWithCapacity() {
     var capacity = document.querySelector('#capacity');
-    var childrens = capacity.children;
-    var forOneQuest = childrens[2];
-    var forTwoQuests = childrens[1];
-    var forThreeQuests = childrens[0];
-    var notForQuests = childrens[3];
+    var capacityChildrens = capacity.children;
+    var roomsNumberValue = parseInt(roomsNumber.value, 10);
 
-    capacity.value = roomNumber.value;
+    for (var i = 0; i < capacityChildrens.length; i++) {
+      var capacityOption = capacityChildrens[i];
+      var capacityOptionValue = parseInt(capacityOption.value, 10);
+      capacityOption.removeAttribute('selected');
 
-    for (var i = 0; i < childrens.length; i++) {
-      childrens[i].setAttribute('disabled', null);
-    }
+      var isDisabled = false;
+      var isSelected = false;
+      if (roomsNumberValue < 100) {
+        isDisabled = capacityOptionValue > roomsNumberValue || capacityOptionValue === 0;
+        isSelected = capacityOptionValue === roomsNumberValue;
+      } else {
+        isDisabled = capacityOptionValue !== 0;
+        isSelected = !isDisabled;
+      }
 
-    if (roomNumber.value === '1') {
-      forOneQuest.removeAttribute('disabled');
-    } else if (roomNumber.value === '2') {
-      forOneQuest.removeAttribute('disabled');
-      forTwoQuests.removeAttribute('disabled');
-    } else if (roomNumber.value === '3') {
-      forOneQuest.removeAttribute('disabled');
-      forTwoQuests.removeAttribute('disabled');
-      forThreeQuests.removeAttribute('disabled');
-    } else {
-      notForQuests.removeAttribute('disabled');
-      capacity.value = '0';
+      if (isDisabled) {
+        capacityOption.setAttribute('disabled', '');
+      } else {
+        capacityOption.removeAttribute('disabled');
+      }
+
+      if (isSelected) {
+        capacityOption.setAttribute('selected', isSelected);
+      }
     }
   }
 
@@ -330,7 +333,7 @@
 
   timeIn.addEventListener('change', syncTimeIn);
   housingType.addEventListener('change', syncTypeWithPrice);
-  roomNumber.addEventListener('change', syncRoomsWithCapacity);
+  roomsNumber.addEventListener('change', syncRoomsWithCapacity);
 
   var offers = getOffers(8);
   renderPinMapElements(offers);
