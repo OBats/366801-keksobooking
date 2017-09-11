4a7d44c693898db2c1b3f60b9bcd6211248426ab
OBats <orest.bats@gmail.com>
2017-09-11 17:31:47 +0300

    Divided functions

--- js/form.js
+++ js/form.js
@@ -1,6 +1,68 @@
 'use strict';
 
 (function () {
+  var timeInElement = document.querySelector('#timein');
+  var timeOutElement = document.querySelector('#timeout');
+  var housingTypeElement = document.querySelector('#type');
+  var roomsNumberElement = document.querySelector('#room_number');
+  var guestsNumberElement = document.querySelector('#capacity');
+  var pricePerNightElement = document.querySelector('#price');
+
+  function syncOptionsValues(element, value) {
+    element.value = value;
+  }
+
+  function syncMinValue(element, value) {
+    element.min = value;
+  }
+
+  function syncRoomsWithGuests(element, enabledOptions) {
+    var options = [].slice.call(element.querySelectorAll('option'));
+    options.forEach(function (option) {
+      var optionValueIdx = enabledOptions.indexOf(option.value);
+      if (optionValueIdx > -1) {
+        option.disabled = false;
+        if (optionValueIdx === 0) {
+          option.selected = true;
+        }
+      } else {
+        option.disabled = true;
+      }
+    });
+  }
+
+  window.synchronizeFields(
+      timeInElement,
+      timeOutElement,
+      ['12:00', '13:00', '14:00'],
+      ['12:00', '13:00', '14:00'],
+      syncOptionsValues
+  );
+
+  window.synchronizeFields(
+      housingTypeElement,
+      pricePerNightElement,
+      ['flat', 'house', 'palace', 'bungalo'],
+      ['1000', '5000', '10000', '0'],
+      syncMinValue
+  );
+
+  window.synchronizeFields(
+      timeInElement,
+      timeOutElement,
+      ['12:00', '13:00', '14:00'],
+      ['12:00', '13:00', '14:00'],
+      syncOptionsValues
+  );
+
+  window.synchronizeFields(
+      roomsNumberElement,
+      guestsNumberElement,
+      ['1', '2', '3', '100'],
+      [['1'], ['2', '1'], ['3', '2', '1'], ['0']],
+      syncRoomsWithGuests
+  );
+
   window.form = {
     setAddress: function (coords) {
       var addressInputElement = document.querySelector('#address');
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,13 +1,7 @@
 'use strict';
 
 (function () {
-  var timeInElement = document.querySelector('#timein');
-  var timeOutElement = document.querySelector('#timeout');
-  var housingTypeElement = document.querySelector('#type');
-  var roomsNumberElement = document.querySelector('#room_number');
-  var guestsNumberElement = document.querySelector('#capacity');
-
-  function synchronizeFields(syncSource, syncTarget, sourceOptions, targetOptions, syncFn) {
+  window.synchronizeFields = function (syncSource, syncTarget, sourceOptions, targetOptions, syncFn) {
     function sync() {
       var selectedValue = syncSource.value;
       var selectedOptionIdx = sourceOptions.indexOf(selectedValue);
@@ -22,70 +16,5 @@
     return function () {
       syncSource.removeEventListener('change', sync);
     };
-  }
-
-  function syncValues(element, value) {
-    element.value = value;
-  }
-
-  function syncRoomsWithCapacity(element, enabledOptions) {
-    var options = [].slice.call(element.querySelectorAll('option'));
-    options.forEach(function (option) {
-      var optionValueIdx = enabledOptions.indexOf(option.value);
-      if (optionValueIdx > -1) {
-        option.disabled = false;
-        if (optionValueIdx === 0) {
-          option.selected = true;
-        }
-      } else {
-        option.disabled = true;
-      }
-    });
-  }
-
-  function syncTypeWithPrice() {
-    var pricePerNight = document.querySelector('#price');
-
-    switch (housingTypeElement.value) {
-      case 'flat':
-        pricePerNight.min = '1000';
-        break;
-      case 'house':
-        pricePerNight.min = '5000';
-        break;
-      case 'palace':
-        pricePerNight.min = '10000';
-        break;
-      default:
-        pricePerNight.min = '0';
-        break;
-    }
-    pricePerNight.value = pricePerNight.value || pricePerNight.min;
-  }
-
-  housingTypeElement.addEventListener('change', syncTypeWithPrice);
-
-  synchronizeFields(
-      timeInElement,
-      timeOutElement,
-      ['12:00', '13:00', '14:00'],
-      ['12:00', '13:00', '14:00'],
-      syncValues
-  );
-
-  synchronizeFields(
-      timeInElement,
-      timeOutElement,
-      ['12:00', '13:00', '14:00'],
-      ['12:00', '13:00', '14:00'],
-      syncValues
-  );
-
-  synchronizeFields(
-      roomsNumberElement,
-      guestsNumberElement,
-      ['1', '2', '3', '100'],
-      [['1'], ['2', '1'], ['3', '2', '1'], ['0']],
-      syncRoomsWithCapacity
-  );
+  };
 })();
