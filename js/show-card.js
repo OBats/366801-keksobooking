7e635f10e3d2bc418002002d8832d79273a9c06e
OBats <orest.bats@gmail.com>
2017-09-14 22:20:21 +0300

    Edited DOM-elements variable's names

--- js/show-card.js
+++ js/show-card.js
@@ -2,10 +2,10 @@
 
 (function () {
   window.showCard = function (offer, targetElement) {
-    var dialogPanel = targetElement.querySelector('.dialog__panel');
+    var dialogPanelElement = targetElement.querySelector('.dialog__panel');
 
     targetElement.querySelector('.dialog__title > img').src = offer.author.avatar;
 
-    targetElement.replaceChild(window.card.createCard(offer), dialogPanel);
+    targetElement.replaceChild(window.card.createCard(offer), dialogPanelElement);
   };
 })();
